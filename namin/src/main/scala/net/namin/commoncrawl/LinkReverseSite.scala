d5235237a0eb75b187f382edd0541d84383a91b4
Nada Amin <namin@alum.mit.edu>
2012-08-14 05:48:13 +0200

    add url filter

--- namin/src/main/scala/net/namin/commoncrawl/LinkReverseSite.scala
+++ namin/src/main/scala/net/namin/commoncrawl/LinkReverseSite.scala
@@ -6,20 +6,26 @@ import spark.SparkContext._
 object LinkReverseSite {
   def main(args: Array[String]) {
     if (args.length == 0) {
-      System.err.println("Usage: LinkReverseSite <host> <segment>")
+      System.err.println("Usage: LinkReverseSite <host> <segment> <urlFilter>")
       System.exit(1)
     }
 
     val segment = if (args.length > 1) args(1) else "1341690147253"
+    val reUrlFilter: String => Boolean = if (args.length > 2) {
+      val urlFilter = args(2).r
+      (x => urlFilter.findFirstIn(x) != None)
+    } else (x => true)
 
     val sc = new SparkContext(args(0), "Generate Link Reverse Site", System.getenv("SPARK_HOME"), List(System.getenv("SPARK_NAMIN_JAR")))
 
-    val s = sc.textFile("s3n://" + System.getenv("AWS_ACCESS_KEY_ID") + ":" + System.getenv("AWS_SECRET_ACCESS_KEY") + "@namin-rev/" + segment).cache
+    val s = sc.textFile("s3n://" + System.getenv("AWS_ACCESS_KEY_ID") + ":" + System.getenv("AWS_SECRET_ACCESS_KEY") + "@namin-rev/" + segment)
 
     for (line <- s) {
       val urls = line.split(" ")
-      val uid = SiteUtils.getUid(urls(0))
-      SiteUtils.s3save("namin-live", "linkrev/" + segment + "/" + uid + ".txt", line)
+      if (reUrlFilter(urls(0))) {
+        val uid = SiteUtils.getUid(urls(0))
+        SiteUtils.s3save("namin-live", "linkrev/" + segment + "/" + uid + ".txt", line)
+      }
     }
 
     System.exit(0)
